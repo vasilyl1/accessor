d23309421c87799e0800a93de3e3f1c0c6b6050f
vasilyl1 <likhovaido@gmail.com>
2024-02-05 13:10:02 -0500

    webpack added with initial login window and tailwind

---
+++ client/src/js/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import PageNotFound from './components/PageNotFound';
+
+// In the main App component, only single instances of Header, Navigtion and Footer
+//  are rendered and several instances of Project
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route
+          path="/"
+          element={<Login />}
+        />
+        <Route
+              path='*'
+              element={<PageNotFound />} 
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const path = require('path');
-const routes = require('./controllers');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '..', '/client', '/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(routes);
+
+require('./routes/htmlRoutes.js')(app);
 
 
 app.listen(PORT, () => {
