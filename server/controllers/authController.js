daef47fa288dcbfbfe4a56554aa67aae4de3bd53
vasilyl1 <likhovaido@gmail.com>
2024-02-09 11:25:10 -0500

    Oauth2.0 authentication complete

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,16 +2,34 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
+passport.serializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, {
+        id: user.googleId,
+        username: user.username,
+        picture: user.picture
+      });
+    });
+  });
+  
+  passport.deserializeUser(function(user, cb) {
+    process.nextTick(function() {
+      return cb(null, user);
+    });
+  });
+
 module.exports = new GoogleStrategy({
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
             callbackURL: `http://localhost:3001/api/auth/google/callback`
         },
             function (accessToken, refreshToken, profile, cb) {
-                const { id, emails } = profile;
+                const { id, emails, name, picture } = profile;
                 const user = {
                     googleId: id,
                     email: emails && emails.length > 0 ? emails[0].value : null, // Access email from profile
+                    username: name,
+                    picture: picture
                 };
                 if (!user.email) {
                     console.log('User profile does not have email field');
--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const path = require('path');
 const routes = require('./routes');
 const app = express();
+const session = require('express-session');
 const PORT = process.env.PORT || 3001;
 const passport = require('passport');
 
@@ -15,6 +16,15 @@ console.log('Passport initialized with GoogleStrategy');
 app.use(express.static(path.join(__dirname, '..', '/client', '/dist')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(session(
+  {
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+  }
+));
+app.use(passport.session()); // this is authorized login session
 
 app.use(routes);
 
