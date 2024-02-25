1b5d49d0f07ba97cedd3509efdb46abcb7553479
vasilyl1 <likhovaido@gmail.com>
2024-02-25 18:57:58 -0500

    google account logout implementation

--- client/src/js/components/UnAuthorized.js
+++ client/src/js/components/UnAuthorized.js
@@ -18,7 +18,7 @@ const UnAuthorized = () => {
                     <p className="mt-6 text-base leading-7 text-gray-600">Sorry, it appears you have authenticated successfully, but do not have permission to access the app resource.</p>
                     <div className="mt-10 flex items-center justify-center gap-x-6">
                         <a
-                            href="/"
+                            href="/logout"
                             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                         >
                             Go back home
